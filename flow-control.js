95980343c277e60074f96f7c6154d3fef1709c11
Tanuka Das <das.tanuka@yahoo.com>
2019-03-22 22:10:05 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -32,7 +32,7 @@ function ternaryTeenager(age) {
 }
 
 function switchAge(age) {
-  switch (age) {
+  /*switch (age) {
     case "13":
       return "You are a teenager"
       break;
@@ -56,9 +56,9 @@ function switchAge(age) {
       break;
     default:
       return "You have an age"
-  }
-/*  switch (age) {
-    case age > 13 || age < 19:
+  }*/
+  switch (age) {
+    case "age > 13 || age < 19":
       console.log("You are a teenager");
       //return "You are a teenager"
       //break;
