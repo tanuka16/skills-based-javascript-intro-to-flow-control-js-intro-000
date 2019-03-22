bbc7bd2d4ba2db45fedf18dfe82d1884292e2465
Tanuka Das <das.tanuka@yahoo.com>
2019-03-22 22:06:59 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -55,7 +55,9 @@ function switchAge(age) {
       return "You are a teenager"
       break;
     default:
-      return "You have an age"
+      return "You are a teenager"
+
+      //return "You have an age"
   }
 /*  switch (age) {
     case age > 13 || age < 19:
